0552a7bc3551894c2dd9f2dbcaf8b36ead04d1b1
Projetos-dos-Alunos <141456283+Projetos-dos-Alunos@users.noreply.github.com>
2023-09-08 10:26:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function addUser(){
+    var nomeJogador1 = document.getElementById("").value;
+    var nomeJogador2 = document.getElementById("").value;
+
+    //localStorage.setItem(chave, valor)
+    localStorage.setItem("", );
+
+    //Quando clicar no botão (função), ir para outra tela.
+
+}
